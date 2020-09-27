c74388b1ebad23f7b8426064dc0a33e23b403810
Ralph Goestenmeier <r14r@via-internet.de>
2020-09-27 18:16:36 +0200

    Initial Version

---
+++ src/app/ng-for/ng-for.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-for',
+  templateUrl: './ng-for.component.html',
+  styleUrls: ['./ng-for.component.css']
+})
+export class NgForComponent implements OnInit {
+
+  names = [
+    "Ana",
+    "John",
+    "Dany",
+    "Bia"
+  ];
+
+  cities = [
+    { name: "São Paulo", state: "SP"},
+    { name: "Porto Alegre", state: "RS"},
+    { name: "Curitiba", state: "PR"},
+    { name: "Rio de Janeiro", state: "RJ"},
+  ];
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
